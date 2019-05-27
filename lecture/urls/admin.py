ca006f53767944b691d52821e621d0fa02d874d5
LiuSunhe <lsunkok@gmail.com>
2019-05-27 13:13:34 +0800

    modified urls/admin.py & views/admin.py

--- lecture/urls/admin.py
+++ lecture/urls/admin.py
@@ -2,8 +2,11 @@ from django.conf.urls import url
 from lecture.views.admin import CreateLectureAPI
 from lecture.views.admin import GetMyLecturesAPI
 from lecture.views.admin import GetLectureByNameAPI
+from lecture.views.admin import EditLectureAPI
+
 urlpatterns = [
     url(r'create-lectures/?$', CreateLectureAPI.as_view(), name="create-lectures"),
     url(r'get-my-lectures/?$', GetMyLecturesAPI.as_view(), name="get-my-lectures"),
     url(r'get-lecture-by-name/?$', GetLectureByNameAPI.as_view(), name="get-lecture-by-name"),
+    url(r'edit-lecture/?$', EditLectureAPI.as_view(), name="edit-lecture"),
 ]
--- lecture/views/admin.py
+++ lecture/views/admin.py
@@ -117,3 +117,25 @@ class EditProblems(APIView):
             return self.success(response_object)
         except Exception as exception:
             return self.error(err=exception, msg=str(exception))
+
+class EditLectureAPI(APIView):
+    response_class = JSONResponse
+    def post(self, request):
+        response_object = dict()
+        # get information from frontend
+        try:
+            lecture_id = int(request.POST.get('lecture_id'))
+            name = request.POST.get('name')
+            description = request.POST.get('description')
+        except Exception as exception:
+            return self.error(err=exception.args, msg="lecture_id:%s\n" % (request.POST.get('lecture_id')))
+        try:
+            # update lecture
+            Lecture.objects.filter(lecture_id=lecture_id).update(name=name, description=description)
+
+            #successful return 
+            response_object["state_code"] = 200 
+            return self.success(response_object)
+        except Exception as exception:
+            return self.error(err=exception, msg=str(exception))
+
